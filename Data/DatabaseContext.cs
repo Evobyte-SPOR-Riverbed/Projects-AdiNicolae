3a39c36a611f5c8b6480081c1e2dcbbf55d49e22
Adi Nicolae <nicolae.adrian.m@gmail.com>
2022-08-15 16:01:17 +0300

    Added seeds into the Glass table, inside the DB.

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -27,4 +27,57 @@ public class DatabaseContext : DbContext
         optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")
                                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."));
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Glass>().HasData(
+            new Glass("Old Fashioned Glass"),
+            new Glass("Rocks Glass"),
+            new Glass("Cosmopolitan Glass"),
+            new Glass("Highball Glass"),
+            new Glass("Collins Glass"),
+            new Glass("Zombie Glass"),
+            new Glass("Weizen Glass"),
+            new Glass("Pilsner Glass"),
+            new Glass("Sling Glass"),
+            new Glass("Pint Glass"),
+            new Glass("Beer Mug"),
+            new Glass("Irish Coffee Glass"),
+            new Glass("Red Wine Glass"),
+            new Glass("White Wine Glass"),
+            new Glass("Balloon Wine Glass"),
+            new Glass("Wine Tasting Glass"),
+            new Glass("Sherry Glass"),
+            new Glass("Champagne Flute"),
+            new Glass("Champahne Coupe"),
+            new Glass("Martini Glass"),
+            new Glass("Margarite Glass"),
+            new Glass("Goblet Glass"),
+            new Glass("Pokal Glass"),
+            new Glass("Milkshake Glass"),
+            new Glass("Hurricane Glass"),
+            new Glass("Poco Grande Glass"),
+            new Glass("Brandy Sniffer"),
+            new Glass("Wobble Cognac Glass"),
+            new Glass("Tulip Whisky Glass"),
+            new Glass("Tulip Glass"),
+            new Glass("Grappa Glass"),
+            new Glass("Pousse Cafe Glass"),
+            new Glass("Cordial Glass"),
+            new Glass("Absinthe Glass"),
+            new Glass("Vodka Glass"),
+            new Glass("Shooter Glass"),
+            new Glass("Shot Glass")
+        );
+
+        modelBuilder.Entity<PreparationMethod>().HasData(
+            new PreparationMethod("Building Method", "It is made by pouring the ingredients one by one into the glass in which it is to be served and then stirred. Ice is added if the recipe calls for it."),
+            new PreparationMethod("Stirring Method", "Stirring refers to the mixing of the ingredients with ice, by stirring quickly in a mixing glass with the stirrer and then straining it into the appropriate glass."),
+            new PreparationMethod("Shaking Method", "It is the mixing of ingredients thoroughly with ice by shaking them in a cocktail shaker and straining them into the appropriate glass."),
+            new PreparationMethod("Blending Method", "The blending method of mixing a cocktail is used for combining fruits, solid foods, ice, etc. in an electric blender. Any drink that can be shaken may be made by blending as well."),
+            new PreparationMethod("Layering Method", "The layering cocktail-making method is used when the ingredients used are of a different color, flavor, and sensitize. One ingredient is floated over the other by pouring gently over the back of a spoon into a small straight-sided glass.")
+        );
+    }
 }
