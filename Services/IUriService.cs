bb36597058be193801d8eba27efce3bd69ee41c8
Adi Nicolae <nicolae.adrian.m@gmail.com>
2022-09-14 12:46:46 +0300

    Base URI Service, checked!

---
+++ Services/IUriService.cs
@@ -0,0 +1,8 @@
+﻿using Drinktionary.Data.Pagination;
+
+namespace Drinktionary.Services;
+
+public interface IUriService
+{
+    public Uri GetPageUri(PaginationFilter paginationFilter, string route);
+}
---
+++ Services/UriService.cs
@@ -0,0 +1,24 @@
+﻿using Drinktionary.Data.Pagination;
+using Drinktionary.Misc;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+
+namespace Drinktionary.Services;
+
+public class UriService : IUriService
+{
+    private readonly string _baseUri;
+
+    public UriService(string baseUri)
+    {
+        _baseUri = baseUri;
+    }
+
+    public Uri GetPageUri(PaginationFilter paginationFilter, string route)
+    {
+        var _enpointUri = new Uri(string.Concat(_baseUri, route));
+        var modifiedUri = QueryHelpers.AddQueryString(_enpointUri.ToString(), nameof(paginationFilter.PageNumber).ToCamelCase(), paginationFilter.PageNumber.ToString());
+        modifiedUri = QueryHelpers.AddQueryString(modifiedUri, nameof(paginationFilter.PageSize).ToCamelCase(), paginationFilter.PageSize.ToString());
+        return new Uri(modifiedUri);
+    }
+}
